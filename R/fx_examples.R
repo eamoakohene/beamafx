686a0ad5f3df60fa057248d5a16199a68c71486e
Emmanuel Amoakohene <eamoakohene@gmail.com>
2016-05-01 00:14:51 +0100

    code update

--- R/fx_examples.R
+++ R/fx_examples.R
@@ -2,10 +2,10 @@
 
 # fx_tbl_tail(n=15, where = "data_code='GBP'", order_by ="yr,mth,dy")
 # fx_tbl_head(n=15, where = "data_code='EUR'", order_by ="yr,mth,dy")
-# beamafx::fx_download$new()$set_data_points(50)$update_euro_all()
+# fx_download$new()$set_data_points(30)$update_euro_all()
 
 
-  # fx_series$new('USD,GBP,EUR')$set_date1('2016/01/01')$set_date2('2016/04/30')$set_freq('d')$get_data()
+  #abc <-  fx_series$new('CNY',codes_only=T)$set_date1('2000/01/01')$set_date2('2016/04/30')$set_freq('d')$get_data()
   # fx_series$new('USD,GBP,EUR')$set_date1('2016/01/01')$set_date2('2016/04/30')$set_freq('m')$build_sql()
   # fx_series$new('USD,GBP,EUR')$set_date1('2015/01/01')$set_date2('2016/04/30')$set_freq('q')$get_data()
   # fx_series$new('USD,GBP,EUR',to='GHS')$set_date1('2015/01/01')$set_date2('2016/04/30')$set_freq('q')$get_data()

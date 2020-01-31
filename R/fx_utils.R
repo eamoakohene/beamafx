69ad0d4b8a4ab0f24e3e1e9f609b789aae6ed6be
Emmanuel Amoakohene <eamoakohene@gmail.com>
2020-01-31 03:56:33 +0000

    Restore to previous
    Restore to previous

--- R/fx_utils.R
+++ R/fx_utils.R
@@ -12,7 +12,7 @@ fx_utils <- R6::R6Class(
     }
 
     ,get_db = function(
-      path=system.file("R","beamafx.sqlite",package="beamafx")
+      path='R:/packages/beamafx/inst/extdata/beamafx.sqlite'
     ){
 
       
@@ -33,7 +33,7 @@ fx_utils <- R6::R6Class(
   ,private = list(
 
 
-    db_pkg =  system.file("R","beamafx.sqlite",package="beamafx"),
+    db_pkg =  system.file("extdata/beamafx.sqlite",package="beamafx"),
     local_mode = TRUE,
 
     get_db_con = function(){
